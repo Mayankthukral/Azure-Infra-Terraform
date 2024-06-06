d8459792d70fe07376d8c694d27099f5c56a8879
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-06 00:04:17 -0400

    added default values to variable and added adress prefix attribute to subnet

--- module/subnet/variable.tf
+++ module/subnet/variable.tf
@@ -1,11 +1,18 @@
 variable "resource_group_name" {
   type = string
+  default = "sampleresourcegroup"
 }
 
 variable "virtual_network_name" {
     type = string
+    default = "samplevnet"
 }
 
 variable "subnet_name" {
     type = string
+    default = "sampleSubnet"
+}
+
+variable "address_prefixes" {
+    default = ["10.0.0.0/16"]
 }
