f484977d5cd8fbc3ee9a2691e437bf0bb685b97f
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-07 21:26:08 -0400

    created variable.tf for VM module

--- module/virtual machine/main.tf
+++ module/virtual machine/main.tf
@@ -1,36 +1,4 @@
 
-variable "vm_subnet" {
-    default = "samplesubnet"
-  
-}
-
-variable "vm_resource_group_name" {
-    default = "sampleresourcegroup"
-  
-}
-
-variable "vm_network_interface_name" {
-    default = "smaplenetworkinterface"
-  
-}
-
-variable "vm_network_interface_location" {
-    default = "westus2"
-  
-}
-
-variable "vm_name" {
-  default = "samplevm"
-}
-
-variable"vm_resource_group_location"{
-    default = "westus2"
-}
-
-variable"vm_password"{
-    
-}
-
 resource "azurerm_network_interface" "main" {
   name                = var.vm_network_interface_name
   location            = var.vm_network_interface_location
---
+++ module/virtual machine/variable.tf
@@ -0,0 +1,31 @@
+variable "vm_subnet" {
+    default = "samplesubnet"
+  
+}
+
+variable "vm_resource_group_name" {
+    default = "sampleresourcegroup"
+  
+}
+
+variable "vm_network_interface_name" {
+    default = "smaplenetworkinterface"
+  
+}
+
+variable "vm_network_interface_location" {
+    default = "westus2"
+  
+}
+
+variable "vm_name" {
+  default = "samplevm"
+}
+
+variable"vm_resource_group_location"{
+    default = "westus2"
+}
+
+variable"vm_password"{
+    
+}
