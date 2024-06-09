4aedb299eaae84a02ca5936198cd01922d297863
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-08 21:41:39 -0400

    added Network security group module

---
+++ module/Network security group/variable.tf
@@ -0,0 +1,12 @@
+variable "nsg_name" {
+    default = "samplensg"
+}
+
+variable "nsg_resource_group_location" {
+    default = "westus2"
+  
+}
+
+variable "nsg_resource_group_name" {
+  default = "sampleresourcegroup"
+}
