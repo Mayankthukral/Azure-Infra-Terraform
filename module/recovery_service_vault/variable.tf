d1a1746d8e00ff7714d59da1656355f5e7b0e33d
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-10 00:17:52 -0400

    added variable.tf to recovery service vault

---
+++ module/recovery_service_vault/variable.tf
@@ -0,0 +1,11 @@
+variable "rescovery_service_vault_resource_group_loctaion" {
+  default = "westus2"
+}
+
+variable "rescovery_service_vault_resource_group_name" {
+  default = "sampleresourcegroup"
+}
+
+variable "recovery_service_vault_name" {
+  default = "sampleRecoveryservicevault"
+}
