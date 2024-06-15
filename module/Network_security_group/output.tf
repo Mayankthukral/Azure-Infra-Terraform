9720ab958be48d60907305dc347208d95487ca3d
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-14 22:54:38 -0400

    defined output for NSG resource

---
+++ module/Network_security_group/output.tf
@@ -0,0 +1,4 @@
+output "module_NSg_id" {
+    value = azurerm_network_security_group.VM_scale_set_NSG.id
+  
+}
---
+++ module/Network_security_group/variable.tf
@@ -0,0 +1,12 @@
+variable "network_security_group_name" {
+  default = "vm_scaleset_NSG"
+}
+
+variable "network_security_group_resource_group_location" {
+  default = "westus2"
+}
+
+variable "network_security_group_resource_group_name" {
+  default = "sampleresourcegroup"
+}
+
