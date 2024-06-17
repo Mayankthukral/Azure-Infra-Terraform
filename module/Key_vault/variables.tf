9241218a3bac5940475514e3697cbc3c72f78aa3
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-16 23:57:05 -0400

    declared variabled to be used in key_vault module

---
+++ module/Key_vault/variables.tf
@@ -0,0 +1,16 @@
+variable "Key_vault_name" {
+  default = "sampleKeyvault"
+}
+
+variable "Key_vault_resourcegroup_location" {
+  default = "uswest2"
+}
+
+variable "Key_vault_resourcegroup_name" {
+  default = "sampleresourcegroup"
+}
+
+variable "ssh_Key_name" {
+  default = "sampleSshKEy"
+}
+
