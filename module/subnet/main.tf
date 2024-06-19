2389362ac6e8b9e8cedca6826657c9f0d79e215e
Mayankthukral <mayankthukral1810@gmail.com>
2024-06-18 23:32:32 -0400

    used for each loop to iterate over Subnet map Variable

--- module/subnet/main.tf
+++ module/subnet/main.tf
@@ -1,7 +1,7 @@
-resource "azurerm_subnet" "subnet1" {
-    name = var.subnet_name
-    resource_group_name = var.resource_group_name
-    virtual_network_name = var.virtual_network_name
-    address_prefixes = var.address_prefixes
+resource "azurerm_subnet" "subnets" {
+  for_each             = var.subnets
+  name                 = each.value.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = [each.value.address_prefix]
 }
-
